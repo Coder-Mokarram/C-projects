4adbc6de7b23db926ae26827a247dd2e62fa9a27
Coder-Mokarram <khanmokarram211@gmail.com>
2025-10-20 18:09:40 +0530

    Update loop.c

--- loop.c
+++ loop.c
@@ -1,11 +1,11 @@
-// #include <stdio.h>
+#include <stdio.h>
 
-// int main(){
-//     int n = 10;
-//     int sum = 0;
-//     for (int i=1;i<=n;i++){
-//          sum = sum+i;
-//     }
-//     printf("%d",sum);
-//     return 0;
-// }
+int main(){
+    int n = 10;
+    int sum = 0;
+    for (int i=1;i<=n;i++){
+         sum = sum+i;
+    }
+    printf("%d",sum);
+    return 0;
+}
